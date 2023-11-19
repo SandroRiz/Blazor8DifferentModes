c628a98e40457be3fade15cfab9622401d97ba34
Sandro Rizzetto <sandro.rizzetto@microgate.it>
2023-11-19 15:05:17 +0100

    Add project files.

---
+++ BlazorApp12_WebAssembly_AspNetCoreHosted_Net7/Program.cs
@@ -0,0 +1,11 @@
+using BlazorApp12_WebAssembly_AspNetCoreHosted_Net7;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
